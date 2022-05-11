f2b370505112a73fb38872bd7730c9cf9f97e5b8
Brian Lewis <brian@fourkitchens.com>
2022-05-11 15:15:37 -0500

    fix: support prerelease builds

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ['main'],
+  branches: ['main', { name: '*', prerelease: true }],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
